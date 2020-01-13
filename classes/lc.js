52099ace53660936740bfcbdaf02f36563b0c471
Nathan Carter <ncarter@bentley.edu>
2020-01-13 16:22:45 -0700

    Define Statement/Environment nesting rules

--- classes/lc.js
+++ classes/lc.js
@@ -2,9 +2,17 @@
 const { Structure } = require( '../dependencies/structure.js' )
 
 class LC extends Structure {
+  insertChild ( child, beforeIndex = 0 ) {
+    if ( child instanceof LC )
+      Structure.prototype.insertChild.call( this, child, beforeIndex )
+  }
 }
 
 class Statement extends LC {
+  insertChild ( child, beforeIndex = 0 ) {
+    if ( child instanceof Statement )
+      LC.prototype.insertChild.call( this, child, beforeIndex )
+  }
 }
 
 class Environment extends LC {
--- index.js
+++ index.js
@@ -6,3 +6,24 @@ console.log( 'Checking to be sure all defined classes exist...' )
 console.log( 'LC?', !!LC )
 console.log( 'Statement?', !!Statement )
 console.log( 'Environment?', !!Environment )
+
+console.log( 'Checking to be sure I can build instances...' )
+console.log( 'LC?', !!new LC() )
+console.log( 'Statement?', !!new Statement() )
+console.log( 'Environment?', !!new Environment() )
+
+console.log( 'Checking to be sure I can build trees...' )
+console.log( 'LC in LC?', new LC( new LC() ).children().length )
+console.log( 'LCs in LC?', new LC( new LC(), new LC() ).children().length )
+console.log( 'Statement in Statement?',
+             new Statement( new Statement() ).children().length )
+console.log( 'Statements in Statement?',
+             new Statement( new Statement(), new Statement() ).children().length )
+console.log( 'Environment in Environment?',
+             new Environment( new Environment() ).children().length )
+console.log( 'Environment in Environment?',
+             new Environment( new Environment(), new Environment() ).children().length )
+console.log( 'Statement in Environment?',
+             new Environment( new Statement() ).children().length )
+console.log( 'Environment in Statement?',
+             new Statement( new Environment() ).children().length )
