9dfeaf072cc68f1eb112bbc1d1e1684c4e252ed8
Ken Monks <monks@scranton.edu>
2020-01-14 00:26:38 -0700

    Update conclusions.js

--- tests/conclusions.js
+++ tests/conclusions.js
@@ -2,7 +2,7 @@
 const { OM } = require( '../dependencies/openmath.js' )
 const { LC, Statement, Environment } = require( '../classes/lc.js' )
 
-var A, B, C, outer
+var A, B, C, D, _A, _B, _C, _D, outer
 
 // How to print conclusion lists
 function printcons ( lc ) {
@@ -15,54 +15,82 @@ function ident ( name, given = false ) {
   result.isAGiven = given
   return result
 }
+// report Conclusions
+function report( lc , ...ABCs ) {
+  console.log( 'Conclusions of '+lc+' are',printcons( lc ) )
+  for (var n=0; n<ABCs.length; n++) {
+    console.log( ' - Does '+ABCs[n]+' think it is a conclusion?', ABCs[n].isAConclusionIn( lc ) )
+  }
+}
+// abbreviation for Environments
+function Env ( ...args ) {
+  return new Environment( ...args )
+}
+// make Given Environments
+function _Env( ...args ) {
+  var env = new Environment( ...args )
+  env.isAGiven = true
+  return env
+}
+
+// for now, make reusable A, B, C, :A, :B, :C
+A = ident( 'A' )
+B = ident( 'B' )
+C = ident( 'C' )
+D = ident( 'D' )
+_A = ident( 'A' , true )
+_B = ident( 'B' , true )
+_C = ident( 'C' , true )
+_D = ident( 'D' , true )
 
 // In { A B }, both are conclusions.
-outer = new Environment( A = ident( 'A' ), B = ident( 'B' ) )
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
-console.log( '\tDoes A think it is a concl?', A.isAConclusionIn( outer ) )
-console.log( '\tDoes B think it is a concl?', B.isAConclusionIn( outer ) )
+outer = Env(A,B)
+report( outer, A, B )
 
 // In :{ A B }, both are conclusions.
-outer = new Environment( ident( 'A' ), ident( 'B' ) )
-outer.isAGiven = true
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
+outer = _Env(A,B)
+report( outer, A, B )
 
 // In :{ :A B }, only B is a conclusion.
-outer = new Environment( ident( 'A', true ), ident( 'B' ) )
-outer.isAGiven = true
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
+outer = _Env( _A , B )
+report(outer , _A , B)
 
 // In :{ A :B }, only A is a conclusion.
-outer = new Environment( ident( 'A' ), ident( 'B', true ) )
-outer.isAGiven = true
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
+outer = _Env( A , _B )
+report(outer , A , _B)
 
 // In :{ :A :B }, there are no conclusions
-outer = new Environment( ident( 'A', true ), ident( 'B', true ) )
-outer.isAGiven = true
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
+outer = _Env( _A , _B )
+report( outer , _A , _B )
 
 // In { { A } }, there is one conclusion, A
-outer = new Environment( new Environment( ident( 'A' ) ) )
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
+outer = Env( Env( A ) )
+report( outer, A )
 
 // In { :{ A } }, there are no conclusions
-B = new Environment( ident( 'A' ) )
-B.isAGiven = true
-outer = new Environment( B )
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
+outer = Env( _Env( A ) )
+report( outer, A )
 
 // In { { :A } }, there are no conclusions
-outer = new Environment( new Environment( ident( 'A', true ) ) )
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
+outer = Env( Env( _A ) )
+report( outer, _A )
 
 // In { { :A B } }, the one conclusion is B
-outer = new Environment( new Environment( ident( 'A', true ), ident( 'B' ) ) )
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
+outer = Env( Env( _A , B ) )
+report( outer, _A , B )
 
 // In { { :A B } C }, the conclusions are B, C
-outer = new Environment(
-  new Environment( ident( 'A', true ), ident( 'B' ) ),
-  ident( 'C' )
-)
-console.log( 'Conclusions of '+outer+' are', printcons( outer ) )
+outer = Env( Env( _A , B ) , C )
+report( outer, _A , B, C )
+
+// In { :{ :A B } C }, the conclusion is C only
+outer = Env( _Env( _A , B ) , C )
+report( outer, _A , B, C )
+
+// In { { { :A B } :{ :C D } } :D } the conclusion is B
+outer = Env(  Env( Env(_A,B) , _Env(_C,D) ) , _D  )
+report(outer, _A, B, _C, D, _D)
+
+// In { { :{ :A B } { C :D } } D } the conclusions are C, D
+outer = Env(  Env( _Env(_A,B) , Env(C,_D) ) , D  )
+report(outer, _A, B, C, D, _D)
