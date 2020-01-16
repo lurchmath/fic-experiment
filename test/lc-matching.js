475dc23b2e061d2c3c2e4c47a6a738c92a156c02
Nathan Carter <ncarter@bentley.edu>
2020-01-16 11:28:12 -0700

    Improve matching tests

--- test/lc-matching.js
+++ test/lc-matching.js
@@ -6,33 +6,69 @@ let expect = require( 'expect.js' )
 
 // import all classes defined in this repo
 const { Matcher } = require( '../classes/matching.js' )
-const { LC } = require( '../classes/lc.js' )
+const { LC, Statement } = require( '../classes/all.js' )
+
+// utility function
+// 1. mark as metavars, in pattern, all the metavars listed in the 2nd argument.
+// 2. match pattern against expression and get a list of solutions.
+// 3. ensure that list is the same as the expectations array, which should
+//    contains >=0 objects, each of the form { metavar:instantiation, ... }.
+let check = ( pattern, metavars, expression, expectations ) => {
+  // if the pattern/expression is a string, parse it to an LC
+  if ( !( pattern instanceof LC ) ) pattern = LC.fromString( pattern )
+  if ( !( expression instanceof LC ) ) expression = LC.fromString( expression )
+  // do the same for every value in the expectations
+  expectations.map( expectation => {
+    for ( let key in expectation )
+      if ( expectation.hasOwnProperty( key ) )
+        if ( !( expectation[key] instanceof LC ) )
+          expectation[key] = LC.fromString( expectation[key] )
+  } )
+  // make sure all the metavariables in the pattern are marked as metavariables
+  let markMetavars = ( lc ) => {
+    if ( lc instanceof Statement && metavars.indexOf( lc.identifier ) > -1 )
+      lc.isAMetavariable = true
+    lc.children().map( markMetavars )
+  }
+  markMetavars( pattern )
+  // create a matcher
+  let M = new Matcher()
+  // add the one main constraint we're supposed to test
+  M.addConstraint( pattern, expression )
+  // run it
+  solutions = M.getSolutions()
+  // test it
+  expect( solutions ).to.be.an( Array )
+  expect( solutions ).to.have.length( expectations.length )
+  for ( let i = 0 ; i < expectations.length ; i++ ) {
+    let solution = solutions[i]
+    let expectation = expectations[i]
+    expect( solution ).to.have.length( Object.keys( expectation ).length )
+    for ( let j = 0 ; j < solution.length ; j++ ) {
+      expect( solution[j].pattern ).to.be.a( Statement )
+      let p = solution[j].pattern.identifier
+      expect( p ).to.be.ok()
+      let e = solution[j].expression
+      expect( e ).to.be.an( LC )
+      expect( expectation.hasOwnProperty( p ) ).to.be( true )
+      expect( expectation[p].equals( e ) ).to.be( true )
+    }
+  }
+}
 
 suite( 'Matcher', () => {
 
-  test( 'This will surely fall completely apart', () => {
-    expect( Matcher ).to.be.ok()
-    let M = new Matcher()
-    expect( M ).to.be.a( Matcher )
-    let pattern = LC.fromString( 'plus(A,B)' )
-    pattern.children()[0].isAMetavariable = true // A
-    pattern.children()[1].isAMetavariable = true // B
-    let expression = LC.fromString( 'plus(x,y)' )
-    M.addConstraint( pattern, expression )
-    let S = M.getSolutions()
-    expect( S ).to.be.an( Array )
-    expect( S ).to.have.length( 1 )
-    let S1 = S[0]
-    expect( S1 ).to.be.an( Array )
-    expect( S1 ).to.have.length( 2 )
-    let metavarA = pattern.children()[0]
-    expect( S1[0].pattern.equals( metavarA ) ).to.be( true )
-    expect( S1[0].expression.equals( LC.fromString( 'x' ) ) ).to.be( true )
-    let metavarB = pattern.children()[1]
-    expect( S1[1].pattern.equals( metavarB ) ).to.be( true )
-    expect( S1[1].expression.equals( LC.fromString( 'y' ) ) ).to.be( true )
+  test( 'One solution to plus(A,B) matching plus(x,y) (metavars A,B)', () => {
+    check( 'plus(A,B)', [ 'A', 'B' ], 'plus(x,y)', [ { A : 'x', B : 'y' } ] )
   } )
 
-  // More tests should go here.
+  test( 'No solutions to plus(A,A) matching plus(x,y) (metavar A)', () => {
+    check( 'plus(A,A)', [ 'A' ], 'plus(x,y)', [ ] )
+  } )
+
+  test( 'One solution to A(B,c,D) matching hi(x,c,D) (metavars A,B,D)', () => {
+    check( 'A(B,c,D)', [ 'A', 'B', 'D' ], 'hi(x,c,D)',
+           [ { A : 'hi', B : 'x', D : 'D' } ] )
+  } )
 
 } )
